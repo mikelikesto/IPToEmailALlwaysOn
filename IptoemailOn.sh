9fefab5782444bd4e51aacf2ab328f4f893812f0
mikelikesto <66426421+mikelikesto@users.noreply.github.com>
2022-08-29 19:10:52 +1000

    Update IptoemailOn.sh

--- IptoemailOn.sh
+++ IptoemailOn.sh
@@ -1,7 +1,7 @@
 result=$(curl ifconfig.me)
 result2=$(cat ip.txt)
 
-if [ $result == result2 ]; then 
+if [ $result == $result2 ]; then 
   echo "nothing"
 else
   echo -e 'Subject: IP\n\n '"$(curl ifconfig.me)"'' | sendmail -v email@gmail.com
